568708b170c676dea85a098f28148cfb895d566f
Reggie <reggie.senal@gmail.com>
2024-08-05 10:45:16 +0800

    update with rest security by jpa hibernate

--- src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,42 +1,33 @@
 package com.luv2code.springboot.cruddemo.security;
 
+import com.luv2code.springboot.cruddemo.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
 
-    // add support for JDBC ... no more hardcoded users :-)
-
+    //beans
+    //bcrypt bean definition
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        // define query for usernames
-        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
-        // define query for authorities/roles by username
-        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
-        // nothing matches with the default tables schemas
-        // note on the 'active' (TINYINT) column
-        /* authorities
-            - username
-            - authority
-           users
-            - username
-            - password
-            - enabled
-         */
-
-        return jdbcUserDetailsManager;
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
+    //authenticationProvider bean definition
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService) {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService); //set the custom user details service
+        auth.setPasswordEncoder(passwordEncoder()); //set the password encoder - bcrypt
+        return auth;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
@@ -46,65 +37,15 @@ public class DemoSecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/api/customers").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/customers/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/customers").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/customers").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/customers/**").hasRole("ADMIN")
-                        .requestMatchers(HttpMethod.GET, "/api/members").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.GET, "/api/members/**").hasRole("EMPLOYEE")
-                        .requestMatchers(HttpMethod.POST, "/api/members").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.PUT, "/api/members").hasRole("MANAGER")
-                        .requestMatchers(HttpMethod.DELETE, "/api/members/**").hasRole("ADMIN")
-        );
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN"));
 
         // use HTTP Basic authentication
         http.httpBasic(Customizer.withDefaults());
 
         // disable Cross Site Request Forgery (CSRF)
-        // in general, not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
+        // in general not required for stateless REST APIs that use POST, PUT, DELETE and/or PATCH
         http.csrf(csrf -> csrf.disable());
 
         return http.build();
     }
-
-/*
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
-    }
-*/
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
